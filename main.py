858d6ab20df8336e81c2bb27f0f9fa7d954f1c59
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-07-19 21:57:44 -0400

    Added code

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-
-aqi_data = pd.read_csv("US_AQI.csv", )
+# Code to trim the original US_AQI.csv file before uploading because it was 600MB 
+#aqi_data = pd.read_csv("US_AQI.csv")
 #aqi_data = aqi_data.drop(aqi_data.columns[[0, 1, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14]], axis=1)
 #aqi_data = aqi_data.loc[aqi_data['Defining Parameter'] == 'PM2.5']
 #aqi_data.to_csv(r'C:/Users/Brayden/Documents/GitHub/us-air-quality-vs-heart-failure-rates/data/data.csv', index = False)
 
-#aqi_data = aqi_data.loc[aqi_data['Defining Parameter'] == 'PM2.5']
-#aqi_data['Date'] = pd.to_datetime(aqi_data['Date'])
-#aqi_data['Year'] = aqi_data['Date'].dt.year
-#aqi_data = aqi_data.drop(aqi_data.columns[[0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]], axis=1)
+aqi_data = pd.read_csv("US_AQI.csv")
 
-#aqi_data.head()
+aqi_data['Date'] = pd.to_datetime(aqi_data['Date'])
+aqi_data['Year'] = aqi_data['Date'].dt.year
+aqi_data = aqi_data.drop(aqi_data.columns[[0, 2]], axis=1)
+
+aqi_data.head()
 
 
 #chf_data = pd.read_csv('heart_failure_stats.txt', sep='/t', lineterminator='/r')
