87736b9ff255dce116b9b70d2009b40dfbafbf12
BrayLaak <47552683+BrayLaak@users.noreply.github.com>
2022-07-19 23:55:31 -0400

    Progress on code

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+def split_by_years(dt):
+    dt['year'] = dt['Date'].dt.year
+    return [dt[dt['year'] == y] for y in dt['year'].unique()]
+
 # Code to trim the original US_AQI.csv file before uploading because it was 600MB 
 #aqi_data = pd.read_csv("US_AQI.csv")
 #aqi_data = aqi_data.drop(aqi_data.columns[[0, 1, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14]], axis=1)
@@ -11,15 +15,22 @@ aqi_data = pd.read_csv("US_AQI.csv")
 aqi_data['Date'] = pd.to_datetime(aqi_data['Date'])
 aqi_data['Year'] = aqi_data['Date'].dt.year
 aqi_data = aqi_data.drop(aqi_data.columns[[0, 2]], axis=1)
+aqi_data = aqi_data.groupby(['Year'])['AQI'].mean()
+#result = aqi_data.groupby(aqi_data['Year'].dt.year.agg({'AQI':})
+aqi_data = aqi_data.round(1)
+
 
 aqi_data.head()
 
 
-#chf_data = pd.read_csv('heart_failure_stats.txt', sep='/t', lineterminator='/r')
-#chf_data = chf_data.loc[chf_data['Cause of death'] == 'Congestive heart failure']
-#chf_data = chf_data.drop(chf_data.columns[[0, 2, 3, 4, 6, 7]], axis=1)
-#chf_data = chf_data.astype({"Year": int, "Deaths": int})
-#chf_data.head()
+
+
+
+chf_data = pd.read_csv('heart_failure_stats.txt', sep='\t', lineterminator='\r')
+chf_data = chf_data.loc[chf_data['Cause of death'] == 'Congestive heart failure']
+chf_data = chf_data.drop(chf_data.columns[[0, 2, 3, 4, 6, 7]], axis=1)
+chf_data = chf_data.astype({"Year": int, "Deaths": int})
+chf_data.head()
 
 
 
